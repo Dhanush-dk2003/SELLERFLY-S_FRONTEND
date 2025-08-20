96218021f686d8b1b168eb310968e9e6a2949371
Dhanush-dk2003 <kumardhanush730@gmail.com>
2025-08-20 12:58:24 +0530

    ex

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
   // baseURL: ' https://sellerfly-s-backend.onrender.com/api',
-  withCredentials: true // Important for cookies
+  withCredentials: true 
 });
 
 export default API;
